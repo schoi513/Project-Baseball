6ec1b370e13c6c65befb24c5315ad1b255821f2f
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-08-17 09:59:39 -0500

    cited the help from Niso Z with the config.js

--- db/config.js
+++ db/config.js
@@ -42,28 +42,7 @@ else if (process.env.NODE_ENV === 'production') {
     dbData = process.env.DATABASE_URL
 }
 
-// const DB_NAME = process.env.DB_NAME || 'the_photobook_dev';
-// const options = {
-//     query: (e) => {
-//         console.log(e.query);
-//     },
-// };
+// Niso Z helped out with the config code!!!
 
-// const pgp = require('pg-promise')(options);
-
-// function setDatabase() {
-//     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-//         return pgp({
-//             database: DB_NAME,
-//             port: 5432,
-//             host: 'localhost',
-//         })
-//     } 
-//     else {
-//         return pgp(process.env.DATABASE_URL)
-//     }
-// }
-
-// module.exports = setDatabase()
 
 module.exports = pgp(dbData)
