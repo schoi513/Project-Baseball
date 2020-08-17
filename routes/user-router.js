a480afe1846c381cae56f8da562fbebd6897f819
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-08-17 09:25:16 -0500

    user router fixed, feature added per MS

--- routes/user-router.js
+++ routes/user-router.js
@@ -5,7 +5,7 @@ const authHelpers = require('../services/auth/auth-helper');
 const usersController = require('../controllers/user-controller');
 const teamsController = require('../controllers/teams-controller');
 
-userRouter.get('/', authHelpers.loginRequired, teamsController.show, usersController.index);
+userRouter.get('/', authHelpers.loginRequired, usersController.index);
 userRouter.get('/new', authHelpers.loginRedirect, (req, res) => {
   res.render('auth/register');
 });
